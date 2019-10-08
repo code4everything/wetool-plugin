79e720853caa9aab70d2d7c86c8bea2bc359b3b8
pantao <easepan@qq.com>
2019-10-08 23:21:21 +0800

    fix: bean factory

--- wetool-plugin-support/src/main/java/org/code4everything/wetool/plugin/support/factory/BeanFactory.java
+++ wetool-plugin-support/src/main/java/org/code4everything/wetool/plugin/support/factory/BeanFactory.java
@@ -2,6 +2,7 @@ package org.code4everything.wetool.plugin.support.factory;
 
 import cn.hutool.core.util.ObjectUtil;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import org.code4everything.boot.base.ReferenceUtils;
 import org.code4everything.wetool.plugin.support.BaseViewController;
 import org.code4everything.wetool.plugin.support.constant.AppConsts;
@@ -17,6 +18,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author pantao
  * @since 2018/3/31
  */
+@Slf4j
 @UtilityClass
 public class BeanFactory {
 
@@ -30,6 +32,12 @@ public class BeanFactory {
      * 注册多例Bean
      */
     public static void register(String name, Object bean) {
+        if (isRegistered(name)) {
+            // 已注册
+            String msg = "key[{}] is already registered, registered class is {}, register failed class: {}";
+            log.warn(msg, name, get(name).getClass().getName(), bean.getClass().getName());
+            return;
+        }
         PROTOTYPE_MAPPING.put(name, new SoftReference<>(bean));
     }
 
@@ -45,11 +53,16 @@ public class BeanFactory {
      * 注册单例Bean
      */
     public static <T> void register(T bean) {
+        if (isRegistered(bean.getClass())) {
+            // 已注册
+            log.warn("class[{}] is already registered", bean.getClass().getName());
+            return;
+        }
         SINGLETON_MAPPING.put(bean.getClass(), bean);
     }
 
     /**
-     * 注册视图Bean
+     * 注册视图Bean，插件请调用下面的 {@link #registerView(String, String, BaseViewController)}方法
      */
     public static void registerView(String tabName, BaseViewController viewController) {
         registerView(AppConsts.Title.APP_TITLE, tabName, viewController);
@@ -59,7 +72,13 @@ public class BeanFactory {
      * 注册视图Bean，插件注册视图请调用此方法，以免冲突
      */
     public static void registerView(String tabId, String tabName, BaseViewController viewController) {
-        register(viewController);
+        String viewName = tabId + tabName;
+        if (isViewRegistered(viewName)) {
+            // 已注册
+            String msg = "key[{}] is already registered, registered view class is {}, register failed class: {}";
+            log.warn(msg, viewName, getView(viewName).getClass().getName(), viewController.getClass().getName());
+            return;
+        }
         VIEW_MAPPING.put(tabId + tabName, new WeakReference<>(viewController));
     }
 
@@ -81,6 +100,16 @@ public class BeanFactory {
     }
 
     /**
+     * 获取视图Bean
+     *
+     * @param viewName 由tabId和tabName拼接而成
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends BaseViewController> T getViewObject(String viewName) {
+        return (T) getView(viewName);
+    }
+
+    /**
      * 单例Bean是否注册
      */
     public static boolean isRegistered(Class<?> clazz) {
@@ -93,7 +122,7 @@ public class BeanFactory {
      * @param viewName 由tabId和tabName拼接而成
      */
     public static boolean isViewRegistered(String viewName) {
-        return VIEW_MAPPING.containsKey(viewName);
+        return VIEW_MAPPING.containsKey(viewName) && ObjectUtil.isNotNull(ReferenceUtils.unwrap(VIEW_MAPPING.get(viewName)));
     }
 
     /**
