a584f756d3d8b9b8df2c705616026ed183a2b6ce
pantao <easepan@qq.com>
2019-11-28 22:00:13 +0800

    feat: parse js expression

--- wetool-plugin-devtool/wetool-plugin-devtool-java/src/main/java/org/code4everything/wetool/plugin/devtool/java/controller/MainController.java
+++ wetool-plugin-devtool/wetool-plugin-devtool-java/src/main/java/org/code4everything/wetool/plugin/devtool/java/controller/MainController.java
@@ -4,12 +4,14 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.RuntimeUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.script.ScriptUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import org.code4everything.wetool.plugin.devtool.java.constant.CommonConsts;
 import org.code4everything.wetool.plugin.support.BaseViewController;
 import org.code4everything.wetool.plugin.support.factory.BeanFactory;
+import org.code4everything.wetool.plugin.support.util.FxDialogs;
 import org.code4everything.wetool.plugin.support.util.FxUtils;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
@@ -28,10 +30,10 @@ public class MainController implements BaseViewController {
     public TextField targetFile;
 
     @FXML
-    public TextArea elResult;
+    public TextArea parsedResult;
 
     @FXML
-    public TextArea elExpression;
+    public TextArea expression;
 
     @FXML
     private void initialize() {
@@ -51,14 +53,27 @@ public class MainController implements BaseViewController {
             FileUtil.writeUtf8String(RuntimeUtil.execForStr("javap", "-c", classFile.getText()), targetFile.getText());
             FxUtils.openFile(targetFile.getText());
         }
-
     }
 
-    public void parseEl() {
-        if (StrUtil.isEmpty(elExpression.getText())) {
+    public void parseSpel() {
+        if (StrUtil.isEmpty(expression.getText())) {
             return;
         }
-        final Object result = parser.parseExpression(elExpression.getText()).getValue();
-        elResult.setText(ObjectUtil.toString(result));
+        try {
+            parsedResult.setText(ObjectUtil.toString(parser.parseExpression(expression.getText()).getValue()));
+        } catch (Exception e) {
+            FxDialogs.showError("表达式不正确！");
+        }
+    }
+
+    public void parseJs() {
+        if (StrUtil.isEmpty(expression.getText())) {
+            return;
+        }
+        try {
+            parsedResult.setText(ObjectUtil.toString(ScriptUtil.eval(expression.getText())));
+        } catch (Exception e) {
+            FxDialogs.showError("表达式不正确！");
+        }
     }
 }
