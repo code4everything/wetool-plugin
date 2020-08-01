c0af606f83c12e709cd52d28107ca255e8081ba7
easepan <easepan@qq.com>
2020-08-01 11:29:41 +0800

    fix: revert delete

---
+++ wetool-plugin-ftpclient/src/main/java/org/code4everything/wetool/plugin/ftp/client/hutool/FtpMode.java
@@ -0,0 +1,17 @@
+package org.code4everything.wetool.plugin.ftp.client.hutool;
+
+/**
+ * FTP连接模式
+ * 
+ * <p>
+ * 见：https://www.cnblogs.com/huhaoshida/p/5412615.html
+ * 
+ * @author looly
+ * @since 4.1.19
+ */
+public enum FtpMode {
+	/** 主动模式 */
+	Active,
+	/** 被动模式 */
+	Passive
+}
