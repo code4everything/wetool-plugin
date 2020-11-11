8d994014a9b07245e233778bd5c7492aef69270b
easepan <easepan@qq.com>
2020-11-11 11:59:05 +0800

    feat: add db ops plugin

---
+++ wetool-plugin-dbops/src/main/java/org/code4everything/wetool/plugin/dbops/WetoolSupporter.java
@@ -0,0 +1,9 @@
+package org.code4everything.wetool.plugin.dbops;
+
+import org.code4everything.wetool.plugin.support.WePluginSupporter;
+
+/**
+ * @author pantao
+ * @since 2020/11/11
+ */
+public class WetoolSupporter implements WePluginSupporter {}
---
+++ wetool-plugin-dbops/src/test/java/org/code4everything/wetool/plugin/dbops/DbOpsTest.java
@@ -0,0 +1,14 @@
+package org.code4everything.wetool.plugin.dbops;
+
+import org.code4everything.wetool.plugin.test.WetoolTester;
+
+/**
+ * @author pantao
+ * @since 2020/11/11
+ */
+public class DbOpsTest {
+
+    public static void main(String[] args) {
+        WetoolTester.runTest(args);
+    }
+}
