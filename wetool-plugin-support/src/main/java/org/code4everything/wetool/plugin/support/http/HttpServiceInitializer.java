5b8b8e4aa9e3f1d647454666e0d855d9560bb139
pantao <easepan@qq.com>
2020-12-06 13:59:54 +0800

    feat: support cors

--- wetool-plugin-support/src/main/java/org/code4everything/wetool/plugin/support/http/HttpServiceInitializer.java
+++ wetool-plugin-support/src/main/java/org/code4everything/wetool/plugin/support/http/HttpServiceInitializer.java
@@ -21,6 +21,8 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+import io.netty.handler.codec.http.cors.CorsConfigBuilder;
+import io.netty.handler.codec.http.cors.CorsHandler;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -37,6 +39,7 @@ public class HttpServiceInitializer extends ChannelInitializer<SocketChannel> {
         ChannelPipeline p = ch.pipeline();
         p.addLast(new HttpServerCodec());
         p.addLast(new HttpObjectAggregator(Integer.MAX_VALUE));
+        p.addLast(new CorsHandler(CorsConfigBuilder.forAnyOrigin().allowCredentials().allowNullOrigin().build()));
         p.addLast(new HttpServerExpectContinueHandler());
         p.addLast(new HttpServiceHandler(port));
     }
