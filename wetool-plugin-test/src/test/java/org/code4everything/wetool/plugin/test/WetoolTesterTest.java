799716199661e45cc1c60a4a3c3e427758c352c2
easepan <easepan@qq.com>
2021-02-22 17:23:15 +0800

    shell package plugin 1.5.0

--- wetool-plugin-test/src/test/java/org/code4everything/wetool/plugin/test/WetoolTesterTest.java
+++ wetool-plugin-test/src/test/java/org/code4everything/wetool/plugin/test/WetoolTesterTest.java
@@ -5,7 +5,7 @@ import org.code4everything.wetool.plugin.support.config.WePluginInfo;
 class WetoolTesterTest {
 
     public static void main(String[] args) {
-        WePluginInfo info = new WePluginInfo("ease", "test", "1.3.0", "1.3.0", "");
+        WePluginInfo info = new WePluginInfo("ease", "test", "1.5.0", "1.5.0", "");
         info.setSupportedClass("org.code4everything.wetool.plugin.test.WetoolSupporter");
         WetoolTester.runTest(info, args);
     }
