eab57d03b74575a5ad2cdcb1a3b5d6bd6c2e4053
pantao <easepan@qq.com>
2021-04-28 22:01:14 +0800

    fix: test import

--- wetool-plugin-test/src/main/java/module-info.java
+++ wetool-plugin-test/src/main/java/module-info.java
@@ -1,15 +1,18 @@
 module org.code4everything.wetool.plugin.test {
     requires org.code4everything.wetool;
     requires org.code4everything.wetool.plugin.support;
-
+    requires core;
     requires java.base;
-
     requires hutool.core;
     requires hutool.system;
     requires hutool.crypto;
-
+    requires hutool.http;
+    requires hutool.cache;
     requires boot.surface;
     requires fastjson;
     requires com.google.common;
     requires FXTrayIcon;
+    requires jnativehook;
+    requires hutool.extra;
+    requires io.netty.codec.http;
 }
